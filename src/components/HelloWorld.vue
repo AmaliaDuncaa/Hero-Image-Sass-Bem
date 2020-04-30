add75355aa3486adc38f45a62bc688a48a2bcc5a
Sam Smith <amalia6.dunca@gmail.com>
2020-04-30 19:13:28 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="continer">
+    <div class="hero">
+      <div class="hero__image hero--size"></div>
+    </div>
+    <div class="box box--grey">
+      <h1 class="box__title">
+        <span class="text--highlight">Invesment</span> in travel
+      </h1>
+      <h2 class="box__subtitle">an invesment in yourself</h2>
+      <p class="box__description">We create soul restoring journeys.</p>
+    </div>
+    <button class="btn">
+      <a href="#" class="btn--purple">Get Started Today</a>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+  @import "../style/main.scss"
+</style>;
